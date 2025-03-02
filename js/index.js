f5e42fab4e1ef04276f6268588eccf33f1e3ef11
lijiankang <3104653373@qq.com>
2025-03-02 19:27:39 +0800

    pwa

--- js/index.js
+++ js/index.js
@@ -227,3 +227,12 @@ class Tip {
 }
 
 window.$Tip = new Tip()
+
+// 注册Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('Service Worker registered:', reg))
+      .catch(err => console.log('Service Worker registration failed:', err));
+  });
+}
